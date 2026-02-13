e50d28d64c7f66a0995ed5bbf6a67b13735e4d80
Muhammad Muaz <FA25-BSE-206@cuilahore.edu.pk>
2026-02-13 12:13:31 +0500

    Initial commit

---
+++ Task 2/Demo.java
@@ -0,0 +1,11 @@
+public class Demo{
+	public static void main(String args[]){
+	Student s1=new Student();
+	s1.name="Muhammad Muaz";
+	s1.regno="FA25-BSE-206";
+	s1.cgpa=3.5;
+	System.out.println("Name: " + s1.name);
+	System.out.println("Registration No. : " + s1.regno);
+	System.out.println("CGPA: " + s1.cgpa);
+}
+}
